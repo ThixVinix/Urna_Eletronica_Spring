b4b413384526b08d51b1e917b33ffb07141fdc74
Juan Ribeiro Silva Pereira <juan.pereira@capgemini.com>
2021-10-07 12:00:48 -0300

    Configuração do backEnd com spring

---
+++ urnaEletronica/src/test/java/br/com/proway/turmab/grupo5/urnaEletronica/UrnaEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proway.turmab.grupo5.urnaEletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
