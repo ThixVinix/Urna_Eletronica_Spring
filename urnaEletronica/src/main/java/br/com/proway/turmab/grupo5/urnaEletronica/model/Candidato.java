6c3e853cfee50bf39fa86486c17ac301626275d8
Borges <natan.borges@capgemini.com>
2021-10-08 16:10:29 -0300

    Mapeamento da tabela Candidatos

--- urnaEletronica/src/main/java/br/com/proway/turmab/grupo5/urnaEletronica/model/Candidato.java
+++ urnaEletronica/src/main/java/br/com/proway/turmab/grupo5/urnaEletronica/model/Candidato.java
@@ -1,31 +1,43 @@
 package br.com.proway.turmab.grupo5.urnaEletronica.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "candidatos")
 public class Candidato {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "nome", columnDefinition = "VARCHAR", unique = false, updatable = true, nullable = false)
 	private String nome;
+	
+	@Column(name = "numero", columnDefinition = "SMALLINT", unique = false, updatable = false, nullable = false)
 	private Integer numero;
+	
+	@Column(name = "url_imagem", columnDefinition = "VARCHAR", unique = false, updatable = true, nullable = false)
 	private String urlImagem;
 
 	public Candidato() {
 
 	}
-	
-	
 
-	public Candidato( String nome, Integer numero, String urlImagem) {
+	public Candidato(String nome, Integer numero, String urlImagem) {
 		super();
 		this.setNome(nome);
 		this.setNumero(numero);
 		this.setUrlImagem(urlImagem);
 	}
 
-
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public String getNome() {
 		return nome;
@@ -50,12 +62,5 @@ public class Candidato {
 	public void setUrlImagem(String urlImagem) {
 		this.urlImagem = urlImagem;
 	}
-
-	
-
-	
-
-
-	
 
 }
